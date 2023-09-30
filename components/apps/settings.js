2d7b15a536570d490f13232c4ae0e1b61a7243ce
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2023-09-30 09:50:00 +0100

    Update settings.js

--- components/apps/settings.js
+++ components/apps/settings.js
@@ -12,7 +12,7 @@ export function Settings(props) {
     "wall-7": "./images/wallpapers/wall-7.webp",
     "wall-8": "./images/wallpapers/wall-8.webp",
     "wall-9": "./images/wallpapers/wall-9.webp",
-    "wall-10": "./images/wallpapers/wall-10.webp",
+    "wall-10": "./images/wallpapers/IMG_20230915_000027%20(1).webp",
 
   };
 
