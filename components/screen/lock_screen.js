3abf80d941955943d0cc42f899d9a99c601b1d4c
Z3r0Gr4v1ty <99024751+ShamyaHaria@users.noreply.github.com>
2023-10-18 17:57:30 +0530

    Update lock_screen.js
    Adding wallpaper 9 to lockscreen

--- components/screen/lock_screen.js
+++ components/screen/lock_screen.js
@@ -11,6 +11,8 @@ export default function LockScreen(props) {
     "wall-6": "./images/wallpapers/wall-6.webp",
     "wall-7": "./images/wallpapers/wall-7.webp",
     "wall-8": "./images/wallpapers/wall-8.webp",
+    "wall-9": "./images/wallpapers/wall-9.webp",
+    "wall-10": "./images/wallpapers/IMG_20230915_000027%20(1).webp",
   };
 
   if (props.isLocked) {
